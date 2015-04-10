35ec22e82cf5061dd6572f0d0717324c7ac2ceb1
baojiwei <baojiwei@baojiwei.com>
2015-04-10 17:26:42 +0800

    决策树
    决策树代码

---
+++ 决策树/trees.py
@@ -0,0 +1,30 @@
+#-*-coding:utf-8-*- 
+from math import log
+
+def calcShannonEnt(dataSet):
+    numEntries=len(dataSet)
+    labelCounts={}
+    #为所有可能分类创建字典
+    for featVec in dataSet:
+        currentLabel=featVec[-1]
+        #如果不存在keys，创建key
+        if currentLabel not in labelCounts.keys():
+            labelCounts[currentLabel]=0
+        labelCounts[currentLabel]+=1
+    #shannon熵为0
+    shannonEnt=0.0
+    for key in labelCounts:
+        #计算概率
+        prob=float(labelCounts[key])/numEntries
+        #以2为底求对数
+        shannonEnt-=prob*log(prob,2)
+    return shannonEnt
+
+def createDataSet():
+    dataSet=[[1,1,'yes'],
+             [1,1,'yes'],
+             [1,0,'no'],
+             [0,1,'no'],
+             [0,1,'no']]
+    labels=['no surfacing','flippers']
+    return dataSet, labels
